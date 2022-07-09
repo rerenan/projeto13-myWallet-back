8acc2cf35667df10f7f39aaa15e557ce244a48d8
Renan Araujo <ra.carvalho@unesp.br>
2022-07-09 20:12:13 -0300

    persintencia do usuário por 2 meses

--- src/removeInactives.js
+++ src/removeInactives.js
@@ -1,7 +1,7 @@
 import db from "./db.js"
 
 export default async function removeInactives(){
-    const TWO_MINUTES = 2*60*1000
-    await db.collection('sessions').deleteMany({lastStatus:{$lt: (Date.now()-TWO_MINUTES)}})
+    const TWO_MONTHS = 60*24*60*60*1000
+    await db.collection('sessions').deleteMany({lastStatus:{$lt: (Date.now()-TWO_MONTHS)}})
     
 }
